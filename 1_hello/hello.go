032bb890de6458110d1871fbd683c31ae67064e6
omerarsln <omarslanbjk@gmail.com>
2023-02-10 11:24:35 +0300

    video 14

---
+++ 1_hello/hello.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Hello Go")
+	fmt.Println("What is your name?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err == nil {
+		fmt.Println("Hello", name)
+	} else {
+		log.Fatal(err)
+	}
+}
