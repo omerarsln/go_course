66922e013f6198a9ff16afa402b12b9da647a2c2
omerarsln <omarslanbjk@gmail.com>
2023-02-14 14:13:05 +0300

    hangman v1

--- hangmanGame/main.go
+++ hangmanGame/main.go
@@ -5,31 +5,11 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"regexp"
 	"strings"
 	"time"
-	"unicode"
 	"unicode/utf8"
 )
-
-/*
-+---+
-    |
-    |
-    |
-   ===
-
-Secret Word : ______
-Incorrect Guesses :
-
-Guess a Letter : a
-
-Sorry Your Dead! The word is ZOMBIE
-Yes the Secret Word is ZOMBIE
-
-Please Enter Only One Letter
-Please Enter a Letter
-Please Enter a Letter you Haven't Guessed
-*/
 
 var hmArr = [7]string{
 	" +---+\n" +
@@ -74,32 +54,30 @@ var wordArr = [7]string{
 	"ZODIAC", "ZOMBIE", "FLUFF",
 }
 
-// Stores the random word to be guessed
 var randWord string
-
-// Stores all letters guessed
 var guessedLetters []string
-
-// Stores correct guesses
 var correctLetters []string
-
-// Letters guessed that aren't in the randWord
+var correctIndexes []int
 var wrongGuesses []string
+
+var isLetter = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
+
+func contains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
 
 func generateRandomWord() string {
 	seedSecs := time.Now().Unix()
 	rand.Seed(seedSecs)
 
-	return wordArr[rand.Intn(7)+1]
+	return wordArr[rand.Intn(7)]
 }
-func isLetter(s string) bool {
-	for _, r := range s {
-		if !unicode.IsLetter(r) {
-			return false
-		}
-	}
-	return true
-}
+
 func isGuessedBefore(guessedLetters []string, letter string) bool {
 
 	for _, value := range guessedLetters {
@@ -110,21 +88,33 @@ func isGuessedBefore(guessedLetters []string, letter string) bool {
 	return false
 }
 
-func main() {
-	randWord = generateRandomWord()
-	fmt.Println("ADAM ASMACA OYUNUNA HOŞGELDİNİZ")
+func printGame() {
+	fmt.Print("\n\n")
+	fmt.Println(hmArr[len(wrongGuesses)])
 
-	fmt.Println(hmArr[0])
+	for i := 0; i < len(randWord); i++ {
+		if contains(correctIndexes, i) {
+			fmt.Print(string(randWord[i]))
+		} else {
+			fmt.Print("_")
+		}
+	}
+	fmt.Print("\n")
+}
+
+func getUserGuess() {
 	reader := bufio.NewReader(os.Stdin)
-
 	for true {
 		fmt.Print("Harf tahmini giriniz : ")
 		letter, err := reader.ReadString('\n')
-		letter = strings.Trim(letter, " ")
 
-		if err == nil {
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			letter = strings.ToUpper(letter)
+			letter = strings.TrimSpace(letter)
+
 			if utf8.RuneCountInString(letter) > 1 {
-				fmt.Println(utf8.RuneCountInString(letter))
 				fmt.Println("Tek karakter giriniz")
 			} else if !isLetter(letter) {
 				fmt.Println("Harf girmeniz gerekiyor")
@@ -132,10 +122,39 @@ func main() {
 				fmt.Println("Bu karakteri daha önce girdiniz")
 			} else {
 				guessedLetters = append(guessedLetters, letter)
+				if strings.Contains(randWord, letter) {
+					correctLetters = append(correctLetters, letter)
+
+					for i := 0; i < len(randWord); i++ {
+						if string(randWord[i]) == letter {
+							correctIndexes = append(correctIndexes, i)
+						}
+					}
+				} else {
+					wrongGuesses = append(wrongGuesses, letter)
+				}
+				break
 			}
-		} else {
-			fmt.Println(err)
 		}
 	}
+}
 
+func main() {
+	randWord = generateRandomWord()
+	fmt.Println("ADAM ASMACA OYUNUNA HOŞGELDİNİZ")
+	//fmt.Println(randWord)
+
+	for true {
+		printGame()
+		getUserGuess()
+
+		if len(wrongGuesses) == 7 {
+			fmt.Println("Kaybettiniz")
+			break
+		}
+		if len(correctIndexes) == len(randWord) {
+			fmt.Println("Tebrikler")
+			break
+		}
+	}
 }
